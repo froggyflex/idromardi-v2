2b525e8ac11e66bd5dbbd70035f1a605c9873916
froggyflex <andrea.tallaros7@gmail.com>
2026-03-03 00:58:10 +0200

    test

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 
 export const api = axios.create({
-  baseURL: "https://idromardi-v2.onrender.com."  //"http://localhost:4000/api",
+  baseURL: "https://idromardi-v2.onrender.com"  //"http://localhost:4000/api",
 });
