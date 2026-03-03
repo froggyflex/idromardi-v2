b88d1e662de8229a2e08eddf995b08079bc9dc64
froggyflex <andrea.tallaros7@gmail.com>
2026-03-03 02:45:06 +0200

    test

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://idromardi-v2.onrender.com/api"  //"http://localhost:4000/api",
+  baseURL: "https://SHOULD-BREAK-THIS.com/api" //"http://localhost:4000/api",
 });
 
 export default api;
